0f66ab91df89d46363a79a22b0281fc7ba57189e
kwonsye <kwonsye@naver.com>
2020-07-19 17:17:19 +0900

    [Feat] Add first project structure and dependencies
    - add dependencies at gradle
    - create first project structure

---
+++ src/main/java/com/mashup/backend/potato_invitation_project/PotatoInvitationApplication.java
@@ -0,0 +1,11 @@
+package com.mashup.backend.potato_invitation_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotatoInvitationApplication {
+    public static void main(String[] args){
+        SpringApplication.run(PotatoInvitationApplication.class, args);
+    }
+}
