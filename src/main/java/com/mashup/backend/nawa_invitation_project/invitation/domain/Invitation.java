f67257969040c68fd7f7293019ae33b38f258104
JaeHyeonKim19 <JaeHyeonKim19@gmail.com>
2020-07-25 20:16:45 +0900

    [Feat] Implement Invitation Entity and Repository

---
+++ src/main/java/com/mashup/backend/nawa_invitation_project/invitation/domain/Invitation.java
@@ -0,0 +1,67 @@
+package com.mashup.backend.nawa_invitation_project.invitation.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "users")
+@Entity
+@SQLDelete(sql = "UPDATE invitation SET removed_at=NOW() WHERE id =?")
+@Where(clause = "removed_at = null")
+public class Invitation {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String invitationIdentifier;
+
+  private String invitationContents;
+
+  private String invitationTIme;
+
+  private String invitationAddress;
+
+  private String invitationLatitude;
+
+  private String invitationLongitude;
+
+  private String hashedCode;
+
+  private Long usersId;
+
+  private Long templatesId;
+
+  @Builder
+  private Invitation(
+      String invitationIdentifier,
+      String invitationContents,
+      String invitationTIme,
+      String invitationAddress,
+      String invitationLatitude,
+      String invitationLongitude,
+      String hashedCode,
+      Long usersId,
+      Long templatesId
+  ) {
+    this.invitationIdentifier = invitationIdentifier;
+    this.invitationContents = invitationContents;
+    this.invitationTIme = invitationTIme;
+    this.invitationAddress = invitationAddress;
+    this.invitationLatitude = invitationLatitude;
+    this.invitationLongitude = invitationLongitude;
+    this.hashedCode = hashedCode;
+    this.usersId = usersId;
+    this.templatesId = templatesId;
+  }
+}
---
+++ src/main/java/com/mashup/backend/nawa_invitation_project/invitation/domain/InvitationRepository.java
@@ -0,0 +1,7 @@
+package com.mashup.backend.nawa_invitation_project.invitation.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InvitationRepository extends JpaRepository<Invitation, Long> {
+
+}
