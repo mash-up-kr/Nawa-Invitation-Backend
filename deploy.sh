56713fecae079c680740a90eefa5cbcb646f4171
JaeHyeonKim19 <JaeHyeonKim19@gmail.com>
2020-08-28 16:44:17 +0900

    [Fix] Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,20 +2,22 @@
 
 DOCKER_APP_NAME=nawa-invitation-server
 
-EXIST_BLUE=$(sudo /usr/local/bin/docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.yml ps | grep Up)
+EXIST_BLUE=$(/usr/local/bin/docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.yml ps | grep Up)
 
 if [ -z "$EXIST_BLUE" ]; then
     echo "blue up"
-    sudo /usr/local/bin/docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.yml up -d --build nawa-invitation-server-blue
+    /usr/local/bin/docker-compose -f docker-compose.yml up -d --build nawa-invitation-server-blue
 
     sleep 10
 
-    sudo /usr/local/bin/docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.yml down
+    /usr/local/bin/docker-compose stop nawa-invitation-server-green
+    /usr/local/bin/docker-compose rm nawa-invitation-server-green
 else
     echo "green up"
-    sudo /usr/local/bin/docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.yml up -d --build nawa-invitation-server-green
+    /usr/local/bin/docker-compose -f docker-compose.yml up -d --build nawa-invitation-server-green
 
     sleep 10
 
-    sudo /usr/local/bin/docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.yml down
+    /usr/local/bin/docker-compose stop nawa-invitation-server-blue
+    /usr/local/bin/docker-compose rm nawa-invitation-server-blue
 fi
