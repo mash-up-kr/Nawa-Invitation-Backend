b6caaebb3b2ef1c885c89c7bedec71f2fad5708b
JaeHyeonKim19 <JaeHyeonKim19@gmail.com>
2020-10-08 14:58:35 +0900

    [Add] Add manual deploy script for single container

---
+++ single-container-manual-deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+git pull origin develop
+
+echo "clean project"
+./gradlew clean
+
+echo "build project"
+./gradlew build
+
+DOCKER_APP_NAME=nawa-invitation-server
+
+EXIST_BLUE=$(docker ps | grep ${DOCKER_APP_NAME}-blue)
+
+echo "kill and start application"
+
+docker stop ${DOCKER_APP_NAME}-blue
+docker rm ${DOCKER_APP_NAME}-blue
+
+docker-compose -f docker-compose.yml up -d --build ${DOCKER_APP_NAME}-blue
