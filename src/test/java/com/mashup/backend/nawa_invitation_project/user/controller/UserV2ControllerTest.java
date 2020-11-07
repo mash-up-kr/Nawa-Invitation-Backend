1676e3fb381a641ea6fdccf0301997a17dac8236
JaeHyeonKim19 <JaeHyeonKim19@gmail.com>
2020-11-08 04:50:31 +0900

    [Add] Add UserV2ControllerTest

---
+++ src/test/java/com/mashup/backend/nawa_invitation_project/user/controller/UserV2ControllerTest.java
@@ -0,0 +1,34 @@
+package com.mashup.backend.nawa_invitation_project.user.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserV2ControllerTest {
+
+  @Autowired
+  private MockMvc mvc;
+
+  @Test
+  public void deviceIdentifier가_등록된다() throws Exception {
+    mvc.perform(post("/apis/v2/users").header("deviceIdentifier", "abc"))
+        .andExpect(status().isOk());
+  }
+
+  @Test
+  public void deviceIdentifer가_없으면_등록이_안된다() throws Exception {
+    mvc.perform(post("/apis/v2/users").header("deviceIdentifer", ""))
+        .andExpect(status().is4xxClientError());
+  }
+
+}
