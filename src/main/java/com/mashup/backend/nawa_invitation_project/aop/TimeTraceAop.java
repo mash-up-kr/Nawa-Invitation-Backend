3bfc73a0da5553c45cc4ed7099c7d0f83064bc2d
JaeHyeonKim19 <JaeHyeonKim19@gmail.com>
2020-11-08 16:03:28 +0900

    [Add] Add TimeTracing function for every method

---
+++ src/main/java/com/mashup/backend/nawa_invitation_project/aop/TimeTraceAop.java
@@ -0,0 +1,26 @@
+package com.mashup.backend.nawa_invitation_project.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class TimeTraceAop {
+
+  @Around("execution(* com.mashup.backend.nawa_invitation_project..*(..))")
+  public Object execute(ProceedingJoinPoint joinPoint) throws Throwable{
+    long start = System.currentTimeMillis();
+    System.out.println("START: " + joinPoint.toString());
+    try {
+      Object result = joinPoint.proceed();
+      return result;
+    }finally {
+      long finish = System.currentTimeMillis();
+      long timeMs = finish - start;
+      System.out.println("END: " + joinPoint.toString() + " " + timeMs + "ms");
+    }
+
+  }
+}
