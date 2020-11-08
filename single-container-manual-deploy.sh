dd7edb8ff57ea192959124fbff2a654d24f641a0
JaeHyeonKim19 <JaeHyeonKim19@gmail.com>
2020-11-09 00:00:46 +0900

    [Fix] Fix single-container-manual-deploy.sh

--- single-container-manual-deploy.sh
+++ single-container-manual-deploy.sh
@@ -2,6 +2,11 @@
 
 git pull origin develop
 
+echo "kill and start application"
+
+docker stop ${DOCKER_APP_NAME}-blue
+docker rm ${DOCKER_APP_NAME}-blue
+
 echo "clean project"
 ./gradlew clean
 
@@ -11,10 +16,5 @@ echo "build project"
 DOCKER_APP_NAME=nawa-invitation-server
 
 EXIST_BLUE=$(docker ps | grep ${DOCKER_APP_NAME}-blue)
-
-echo "kill and start application"
-
-docker stop ${DOCKER_APP_NAME}-blue
-docker rm ${DOCKER_APP_NAME}-blue
 
 docker-compose -f docker-compose.yml up -d --build ${DOCKER_APP_NAME}-blue
